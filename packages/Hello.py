c136d5544d39db7769f58ed141ae0af94963bdbe
wrodas123 <se.rodas.workicho@gmail.com>
2020-07-23 13:24:12 +0300

    Final first day commit

--- packages/Hello.py
+++ packages/Hello.py
@@ -5,8 +5,51 @@ from telegram.ext import Updater
 import logging  #for debugging purposes
 from telegram.ext import CommandHandler 
 from telegram.ext import MessageHandler, Filters
-from .browsing import googleGoodreads
 import telegram
+import requests
+from bs4 import BeautifulSoup
+import re
+import urllib.parse
+from urllib.parse import urlparse
+
+def googleSearch(query):
+    dict_results = {}
+    g_clean = [ ] #this is the list we store the search results
+    url = 'https://www.google.com/search?client=ubuntu&channel=fs&q={}&ie=utf-8&oe=utf-8'.format(query) #this is the actual query we are going to scrape
+    try:
+            html = requests.get(url)
+            if html.status_code==200:
+                soup = BeautifulSoup(html.text, 'lxml')
+                a = soup.find_all('a') # a is a list
+                for i in a:
+                    k = i.get('href')
+                    try:
+                        m = re.search("(?P<url>https?://[^\s]+)", k)
+                        n = m.group(0)
+                        rul = n.split('&')[0]
+                        domain = urlparse(rul)
+                        if(re.search('google.com', domain.netloc)):
+                            continue
+                        else:
+                            g_clean.append(rul)
+                    except:
+                        continue
+                filtered_results = [x for x in g_clean if 'goodreads.com' in x]
+                
+                for i in filtered_results:
+                    res = requests.get(i)
+                    soup = BeautifulSoup(res.text, 'html.parser')
+                    dict_results[soup.title.getText()] = i
+    except Exception as ex:
+            print(str(ex))
+    finally:
+            return dict_results
+
+def googleGoodreads(search): #TODO: Needs to be much faster
+    results = googleSearch(search + " Goodreads")
+    return results
+
+
 bot = telegram.Bot(token='621697472:AAFIgz7WpxP34D_kSBeSiblYm6ZxjrpC67Q')
 
 
@@ -30,6 +73,11 @@ def echo(update, context):
     received_message = update.message.text
     chat_id = update.effective_chat.id
     print(f'{chat_id}: {received_message}')
+    reply = googleGoodreads(received_message)
+    result = ''
+    for i in reply:
+        result += f'{i} | \n {reply[i]} \n'
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
 def caps(update, context):
     text_caps = ' '.join(context.args).upper()
